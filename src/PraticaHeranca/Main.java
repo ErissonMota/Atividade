2052272c0a04901151cf4652abf91ec8debd8e2c
Erisson Mota <erissonrenato6@gmail.com>
2023-09-27 23:05:58 -0300

    heranca

---
+++ src/PraticaHeranca/Main.java
@@ -0,0 +1,13 @@
+package PraticaHeranca;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Secretario s1 = new Secretario();
+		s1.setNome("Erisson");
+		System.out.println(s1.getNome());
+
+	}
+
+}
